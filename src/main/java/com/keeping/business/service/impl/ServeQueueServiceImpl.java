8cb8875394e56bf216ad001681ffa37fd5688084
zhangleilei <hit-zll@163.com>
2014-11-28 17:10:45 +0800

    ServeQueue

--- src/main/java/com/keeping/business/service/impl/ServeQueueServiceImpl.java
+++ src/main/java/com/keeping/business/service/impl/ServeQueueServiceImpl.java
@@ -7,6 +7,7 @@ import com.keeping.business.common.exception.BusinessServiceException;
 import com.keeping.business.dal.dao.ServeQueueDao;
 import com.keeping.business.dal.model.ServeQueueDo;
 import com.keeping.business.service.ServeQueueService;
+import com.keeping.business.service.converter.ServeQueueConverter;
 import com.keeping.business.web.controller.model.ServeQueue;
 
 public class ServeQueueServiceImpl implements ServeQueueService{
@@ -18,13 +19,7 @@ public class ServeQueueServiceImpl implements ServeQueueService{
 	public void addServeQueue(ServeQueue serveQueue)
 			throws BusinessServiceException {
 		// TODO Auto-generated method stub
-		ServeQueueDo serveQueueDo = new ServeQueueDo();
-		
-		serveQueueDo.setEndTime(serveQueue.getEndTime());
-		serveQueueDo.setOrder_id(serveQueue.getOrder_id());
-		serveQueueDo.setStartTime(serveQueue.getStartTime());
-		serveQueueDo.setStep(serveQueue.getStep());
-		serveQueueDo.setUser_id(serveQueue.getUser_id());
+		ServeQueueDo serveQueueDo = ServeQueueConverter.getServeQueueDo(serveQueue);
 		
 		serveQueueDao.addServeQueue(serveQueueDo);
 	}
@@ -33,18 +28,14 @@ public class ServeQueueServiceImpl implements ServeQueueService{
 			throws BusinessServiceException {
 		// TODO Auto-generated method stub
 		List<ServeQueueDo> serveQueueDoes = serveQueueDao.queryByServeQueuestep(step);
-		
 		List<ServeQueue> serveQueues = new ArrayList<ServeQueue>();
 		
-		for (int i = 0; i < serveQueues.size(); i++){
-			ServeQueue serveQueue = new ServeQueue();
-			serveQueue.setCreateTime(serveQueueDoes.get(i).getCreateTime());
-			serveQueue.setEndTime(serveQueueDoes.get(i).getEndTime());
-			serveQueue.setStartTime(serveQueueDoes.get(i).getStartTime());
-			serveQueue.setStep(serveQueueDoes.get(i).getStep());
-			serveQueue.setOrder_id(serveQueueDoes.get(i).getOrder_id());
-			serveQueue.setUser_id(serveQueueDoes.get(i).getUser_id());
-			serveQueues.add(serveQueue);
+		if (serveQueueDoes == null){
+			return serveQueues;
+		}
+		
+		for (int i = 0; i < serveQueueDoes.size(); i++){
+			serveQueues.add(ServeQueueConverter.getServeQueue(serveQueueDoes.get(i)));
 		}
 		
 		return serveQueues;
