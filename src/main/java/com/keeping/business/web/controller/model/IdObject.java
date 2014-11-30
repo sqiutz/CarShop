54883f177ab261c77a68ded770e0fa1b4826e292
Jay Gu <j.gu@sap.com>
2014-11-30 22:29:36 +0800

    IdObject

---
+++ src/main/java/com/keeping/business/web/controller/model/IdObject.java
@@ -0,0 +1,15 @@
+package com.keeping.business.web.controller.model;
+
+public class IdObject {
+	
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+}
