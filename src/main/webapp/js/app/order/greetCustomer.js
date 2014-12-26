ebe865974c64d58923ca871082d834225dd22e17
Jay Gu <j.gu@sap.com>
2014-12-26 18:05:38 +0800

    queing number

---
+++ src/main/webapp/js/app/order/greetCustomer.js
@@ -0,0 +1,24 @@
+(function($) {
+    applyLang();
+    $.UserInfo.getProperty({
+        data : {
+            name : 'LANGUAGE'
+        },
+        success : function(data) {
+            if (data.code == '000000') {
+                var langCode = data.obj.value;
+                applyLang(langCode);
+            }
+        }
+    });
+    
+    function applyLang(langCode) {
+        if(undefined === langCode || null === langCode) {
+            langCode = 'en_US';
+        }
+        loadLang('lang/' + langCode + '.js', function() {
+            $('#title').text(SUZUKI_SIAGA_LEBIH_MENGERTI_KELUARGA);
+            $('#queingNumber').text(QUEING_NUMBER);
+        });
+    } 
+})(jQuery);
