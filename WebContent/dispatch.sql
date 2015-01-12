6f424af009ef36a26de9f4458f034e1003f37a21
sqiutz <hit-zll@163.com>
2015-01-12 16:26:33 +0800

    SQL

--- WebContent/dispatch.sql
+++ WebContent/dispatch.sql
@@ -99,6 +99,7 @@ CREATE TABLE `TBL_ORDER` (
   `estimationTime` bigint(10) DEFAULT NULL,
   `bookNumber` varchar(48) DEFAULT NULL,
   `is_book` smallint(1) NOT NULL DEFAULT 0, 
+  `assign_date` datetime  NOT NULL DEFAULT '1990-01-01 00:00:00',
   `create_time` timestamp NOT NULL DEFAULT '1990-01-01 00:00:00',
   `start_time` timestamp  NOT NULL DEFAULT '1990-01-01 00:00:00',
   `end_time` timestamp  NOT NULL DEFAULT '1990-01-01 00:00:00',
