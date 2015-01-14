8f7cf609bbf67214a9f9d08042c095c196e61352
sqiutz <hit-zll@163.com>
2015-01-14 15:10:54 +0800

    Time

--- src/main/java/com/keeping/business/web/controller/model/UserWorkload.java
+++ src/main/java/com/keeping/business/web/controller/model/UserWorkload.java
@@ -21,10 +21,6 @@ public class UserWorkload extends BaseQueue{
 	private String comment; 
 	
 	private Timestamp modifyTime;
-
-	private Date startTime;
-	
-	private Date endTime;
 	
 	private Date allocatedTime;
 	
@@ -102,22 +98,6 @@ public class UserWorkload extends BaseQueue{
 
 	public void setModifyTime(Timestamp modifyTime) {
 		this.modifyTime = modifyTime;
-	}
-
-	public Date getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(Date startTime) {
-		this.startTime = startTime;
-	}
-
-	public Date getEndTime() {
-		return endTime;
-	}
-
-	public void setEndTime(Date endTime) {
-		this.endTime = endTime;
 	}
 
 	public Date getAllocatedTime() {
